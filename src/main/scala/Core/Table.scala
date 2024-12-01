7061aaed6bb28e41c665986434ed6c499f165872
moondg <moondg07@outlook.com>
2024-12-01 20:15:53 +0900

    [Chore]: remove unnecessary methods
    data structure of `Table` changed. new design based on List[(KeyRange, Node(=(IP, Port))] enables tailrec `partitioning` function

--- src/main/scala/Core/Table.scala
+++ src/main/scala/Core/Table.scala
@@ -2,17 +2,8 @@ package Core
 
 import Key._
 
-import Network.Network.IPAddr
-import Network.Network.Port
+import Network.Network.Node
 
-object Table {}
-
-class Table(val keyRanges: List[KeyRange], val address: List[(IPAddr, Port)]) {
-  def find(key: Key): (IPAddr, Port) = {
-    keyRanges.zip(address).filter(x => x._1.contains(key)).head._2
-  }
-  def find(record: Record): (IPAddr, Port) = find(new Key(record.key))
-
-  def keyRange(key: Key): KeyRange = keyRanges.filter(_.contains(key)).head
-  def keyRange(record: Record): KeyRange = keyRange(new Key(record.key))
+object Table {
+  type Table = List[(KeyRange, Node)]
 }
