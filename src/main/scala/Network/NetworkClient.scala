0d8126bf899df7deefcbb048c673d27eca7a3525
moondg <moondg07@outlook.com>
2024-12-07 01:00:24 +0900

    [Chore]: split kway merge into Utils.Postlude.scala

--- src/main/scala/Network/NetworkClient.scala
+++ src/main/scala/Network/NetworkClient.scala
@@ -48,8 +48,6 @@ import message.service.{MasterServiceGrpc, WorkerServiceGrpc}
 import message.common.{DataChunk, KeyRange, KeyRangeTableRow, KeyRangeTable}
 import javax.xml.crypto.Data
 
-import java.io.{File, FileOutputStream}
-
 class NetworkClient(
     val master: Node,
     val client: Node,
@@ -107,64 +105,6 @@ class NetworkClient(
   def sendSamples(sample: List[Key], node: Node): Unit = {}
   def sendRecords(records: List[Record], node: Node): Unit = {}
 
-  def kWayMerge(tempFiles: List[String], outputFilePath: String): Int = {
-    lazy val blocks: Array[Block] = tempFiles.map(makeBlockFromFile).toArray
-    var counter: Int = 0
-    var emptyBlock: Int = 0
-    val tournamentTree: Array[(Record, Int)] =
-      Array.fill(blocks.length)(new Record(Key.max, Array.empty), -1)
-
-    val file = new File(outputFilePath)
-    val fileWriter = new FileOutputStream(file, file.exists())
-
-    @tailrec
-    def treePushInit(cnt: Int): Unit = {
-      if (cnt < blocks.length) {
-        tournamentTree.update(
-          cnt,
-          (new Record(blocks(cnt).block.head.key, blocks(cnt).block.head.value), cnt))
-        blocks.update(cnt, new Block(blocks(cnt).block.tail))
-        treePushInit(cnt + 1)
-      }
-    }
-    def findingMinValueIndex(tree: Array[(Record, Int)]): Int = {
-      if (tree.length == 1) 0
-      else {
-        val divideLength = (tree.length + 1) / 2
-        val dividedTree = tree.grouped(divideLength)
-        val first = findingMinValueIndex(dividedTree.next())
-        val second = findingMinValueIndex(dividedTree.next()) + divideLength
-        if (tree(first)._1.key.compare(tree(second)._1.key) < 0) first
-        else second
-      }
-    }
-    @tailrec
-    def merge(): Unit = {
-      val treeIndex = findingMinValueIndex(tournamentTree)
-      val (data, num) = tournamentTree(treeIndex)
-      assert(num >= 0)
-      blocks.update(num, new Block(blocks(num).block.tail))
-      if (blocks(num).block.nonEmpty) {
-        tournamentTree.update(treeIndex, (blocks(num).block.head, num))
-      } else {
-        tournamentTree.update(treeIndex, (new Record(Key.max, Array.empty[Byte]), -1))
-        emptyBlock += 1
-      }
-      fileWriter.write(data.key.key)
-      fileWriter.write(data.value)
-      counter += 1
-      if (emptyBlock < blocks.length) {
-        merge()
-      }
-    }
-
-    treePushInit(0)
-    merge()
-
-    fileWriter.close()
-
-    counter
-  }
   def send_unmatched_data(): Unit = {}
 
   def wait_until_all_data_received(): Unit = {}
